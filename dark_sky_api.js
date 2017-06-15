45dd5e604caaca07f8b9434e93cbcb837a6c48cf
Gene Grilli <gene@grilli.us>
2017-06-15 12:04:06 -0500

    removed api key

--- dark_sky_api.js
+++ dark_sky_api.js
@@ -23,6 +23,7 @@ app.get('/', function (request, response) {
 
 
 app.get('/api', cache('60 minutes'), function (request, response, next) {
+  var DARK = process.env.DARKSKY_KEY;
   var zip_code = request.query.zip_code;
   console.log('Generating new response', zip_code);
   var dbdata;
@@ -33,7 +34,7 @@ app.get('/api', cache('60 minutes'), function (request, response, next) {
     .then(function(resultsArray) {
       console.log('results', resultsArray);
       dbdata = resultsArray[0];
-      return axios.get(`https://api.darksky.net/forecast/8a1d95b8dc652cbcfad9cff1b79e5194/${dbdata.lat},${dbdata.long}`);
+      return axios.get(`https://api.darksky.net/forecast/DARK/${dbdata.lat},${dbdata.long}`);
     })
     .then(function (darksky) {
     	response.json({db: dbdata, ds: darksky.data});
