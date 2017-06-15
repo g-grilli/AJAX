3be134359d5456849e8c2bfec8f0dd884fe90f8b
Gene Grilli <gene@grilli.us>
2017-06-15 09:33:41 -0500

    heroku setup

--- dark_sky_api.js
+++ dark_sky_api.js
@@ -41,7 +41,7 @@ app.get('/api', cache('60 minutes'), function (request, response, next) {
     .catch(next);
 });
 
-
-app.listen(8000, function () {
-  console.log('Listening on port 8000');
+var PORT = process.env.PORT || 8000;
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
 });
